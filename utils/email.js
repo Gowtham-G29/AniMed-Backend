7bd797627502b68dbeb39e5f1402448b06ac3c52
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2024-12-23 22:54:00 +0530

    update :email options

--- utils/email.js
+++ utils/email.js
@@ -5,27 +5,26 @@ const htmlToText = require('html-to-text'); //convert the html to text
 module.exports = class Email {
     constructor(user, url) {
         this.to = user.email,
-        this.firstName = user.name.split(' ')[0];
+            this.firstName = user.name.split(' ')[0];
         this.url = url;
         this.from = process.env.EMAIL_FROM;
-        
+
     }
 
     newTransport() {
         if (process.env.NODE_ENV === 'production') {
-            //sendgrid
-            return 1;
-        }
 
-        return nodemailer.createTransport({
-            host: process.env.EMAIL_HOST,
-            port: process.env.EMAIL_PORT,
-            // service: 'Gmail', // Uncomment if using Gmail, otherwise use host and port
-            auth: {
-                user: process.env.EMAIL_USERNAME,
-                pass: process.env.EMAIL_PASSWORD
-            }
-        });
+
+            return nodemailer.createTransport({
+                host: process.env.EMAIL_HOST,
+                port: process.env.EMAIL_PORT,
+                // service: 'Gmail', // Uncomment if using Gmail, otherwise use host and port
+                auth: {
+                    user: process.env.EMAIL_USERNAME,
+                    pass: process.env.EMAIL_PASSWORD
+                }
+            });
+        }
 
     }
     async send(template, subject) {
