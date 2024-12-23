7508c104727a4061b6a5b5844952a4bb42fe4ff0
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2024-12-23 23:29:22 +0530

    fix:cookie options

--- controllers/authController.js
+++ controllers/authController.js
@@ -24,7 +24,7 @@ exports.signUp = async (req, res, next) => {
         const cookieOptions = {
             expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',  // Only true for production with HTTPS
+            secure: false,  // Only true for production with HTTPS
             sameSite: 'None',  // Use 'None' if cross-origin
             path: '/',
         };
