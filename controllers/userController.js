b50c709107a2e203ea647720ac6fbc0e4580fd5b
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2025-01-02 10:39:50 +0530

    fix: typo

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,6 @@ const fs = require('fs');
 const path = require('path');
 const animalOwner = require('../models/animalOwnerModel');
 const Email = require('../utils/email');
-const animalOwner = require('../models/animalOwnerModel');
 const VetDoctor = require('../models/vetDoctormodel');
 
 
