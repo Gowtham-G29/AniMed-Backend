c7c03b380d7a1589756fc3a56164fcb170a9408d
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2025-02-04 20:58:41 +0530

    fix 13

--- controllers/animalController.js
+++ controllers/animalController.js
@@ -181,14 +181,11 @@ exports.getNearbyAnimalLocations = async (req, res, next) => {
 
         const animalownersID=await animalOwner.find({district:doctor.district}).select('userID');
 
-     
-
         const animals=await Animal.find({userID:animalownersID.userID});
 
         res.status(200).json({
             status: 'success',
             message: 'animalowners',
-            animalownersID,
             animals
         });
 
