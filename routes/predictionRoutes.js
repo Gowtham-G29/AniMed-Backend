71cbbb55f3804237c7809e5950dd7b1c1d18c8ef
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2025-03-03 12:09:34 +0530

    Create the middleware for the disease prediction options

---
+++ routes/predictionRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import multer from "multer";
+import { predictDisease } from "../controllers/predictController.js";
+
+const router = express.Router();
+const upload = multer(); // File upload middleware
+
+router.post('/predict', upload.single("image"), predictDisease);
+
+export default router;
