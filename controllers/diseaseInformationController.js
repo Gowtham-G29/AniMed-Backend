7f3947909c060d74ee77f1850b0af18e80a83249
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2025-03-07 14:15:11 +0530

    Update the controller logic

--- controllers/diseaseInformationController.js
+++ controllers/diseaseInformationController.js
@@ -7,13 +7,30 @@ exports.updateDiseaseInformations=async(req,res,next)=>{
         
         const diseaseFound=await diseaseInformation.findOne({diseaseName:Name});
 
-        if(!diseaseFound){
-            res.status(200).json({
-                status:'success',
-                message:'Disease is new you can update it.',
+        if(diseaseFound){
+            res.status(403).json({
+                status:'fail',
+                message:'Try for another disease',
                 diseaseFound
             })
         }
+
+        const data={...req.body};
+        if(Object.keys(data).length===0){
+            return res.status(400).json({
+                status:'fail',
+                message:'No data is provided'
+            })
+        };
+
+        const updatedInformation=await diseaseInformation.create(req.body);
+
+        return res.status(201).json({
+            status:'Success',
+            message:`The information for the ${data.diseaseName} disease is created Successfully`,
+            updatedInformation
+        })
+        
 
     }catch(error){
         res.status(500).json({
