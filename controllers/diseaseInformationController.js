55c06577328f5d86062ac749355cd8060309a620
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2025-03-07 14:27:01 +0530

    typo

--- controllers/diseaseInformationController.js
+++ controllers/diseaseInformationController.js
@@ -1,41 +1,41 @@
-const diseaseInformation=require('../models/DiseaseInformationModel');
+const diseaseInformation = require('../models/DiseaseInformationModel');
 
 
-exports.updateDiseaseInformations=async(req,res,next)=>{
-    try{
-        const Name=req.body.diseaseName;
-        
-        const diseaseFound=await diseaseInformation.findOne({diseaseName:Name});
+exports.updateDiseaseInformations = async (req, res, next) => {
+    try {
+        const Name = req.body.diseaseName;
 
-        if(diseaseFound){
-            res.status(404).json({
-                status:'fail',
-                message:'Try for another disease',
+        const diseaseFound = await diseaseInformation.findOne({ diseaseName: Name });
+
+        if (diseaseFound) {
+            return res.status(404).json({
+                status: 'fail',
+                message: 'Try for another disease',
                 diseaseFound
             })
         }
 
-        const data={...req.body};
-        if(Object.keys(data).length===0){
+        const data = { ...req.body };
+        if (Object.keys(data).length === 0) {
             return res.status(400).json({
-                status:'fail',
-                message:'No data is provided'
+                status: 'fail',
+                message: 'No data is provided'
             })
         };
 
-        const updatedInformation=await diseaseInformation.create(req.body);
+        const updatedInformation = await diseaseInformation.create(req.body);
 
         return res.status(201).json({
-            status:'Success',
-            message:`The information for the ${data.diseaseName} disease is created Successfully`,
+            status: 'Success',
+            message: `The information for the ${data.diseaseName} disease is created Successfully`,
             updatedInformation
         })
-        
 
-    }catch(error){
+
+    } catch (error) {
         res.status(500).json({
-            status:'fail',
-            message:error.message
+            status: 'fail',
+            message: error.message
         })
     }
 }
