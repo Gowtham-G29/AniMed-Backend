6c0e993c009517783af5d047959dc9f9196f0d91
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2025-03-11 12:42:55 +0530

    typo

--- routes/predictRoutes.js
+++ routes/predictRoutes.js
@@ -2,9 +2,9 @@ import express from "express";
 import multer from "multer";
 import { predictDisease } from "../controllers/predictController.js";  // ✅ Correct ESM import
 
-const router = express.Router();
+const predictRouter = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.post("/predict", upload.single("image"), predictDisease);
+predictRouter.post("/predict", upload.single("image"), predictDisease);
 
-export default predictRoutes; // ✅ Correct ES Module export
+module.exports= predictRouter; // ✅ Correct ES Module export
